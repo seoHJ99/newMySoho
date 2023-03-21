97bf3b9b53f0ab384e8e28645cc392bd0cc27f74
moongjin97 <84491733+moongjin97@users.noreply.github.com>
2023-03-21 09:48:06 +0900

    수정

--- 3.10/src/main/java/com/study/springboot/client/controller/CartController_Ho.java
+++ 3.10/src/main/java/com/study/springboot/client/controller/CartController_Ho.java
@@ -102,6 +102,7 @@ public class CartController_Ho {
                             @RequestParam("sender_phone2") String s_phone2, // 호준
                             @RequestParam("phone1") String phone1,
                             @RequestParam("phone2")String phone2,
+                            @RequestParam(value = "joinCheck", required = false) String joinCheck,
                             Model model,
                             HttpSession session,
                             MemberJoinDto memberDto,
@@ -164,7 +165,8 @@ public class CartController_Ho {
         OrderResponseDto dto = orderService2.findOrderDto(orderIdx);
         model.addAttribute("order",dto);
 
-        if (session.getAttribute("memberID") == null) {
+        System.out.println(joinCheck);
+        if (session.getAttribute("memberID") == null && joinCheck != null && joinCheck.equals("on")) {
             if (bindingResult.hasErrors()) {
                 // DTO에 설정한 message값을 가져온다.
                 String detail = bindingResult.getFieldError().getDefaultMessage();
