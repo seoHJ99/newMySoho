6e0d6ae8e5b36a240badd8bc52910a519c6cfbff
MakingNameIsAnnoying <121778872+MakingNameIsAnnoying@users.noreply.github.com>
2023-04-19 18:11:50 +0900

    실패시 jar이름 의심

--- 3.10/src/main/java/com/study/springboot/Ex15JpaRealDbApplication.java
+++ 3.10/src/main/java/com/study/springboot/Ex15JpaRealDbApplication.java
@@ -2,10 +2,14 @@ package com.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class Ex15JpaRealDbApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(Ex15JpaRealDbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Ex15JpaRealDbApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
+
+    }
 }
